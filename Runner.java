697b5ab76d0a091295b8cc287529308def915db2
Zekken1234 <95679675+Zekken1234@users.noreply.github.com>
2021-12-07 16:56:27 +0800

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class Runner {
+
+    //TESTING SANA MAG COMMIT ATA?
     public static void main (String [] args) {
         Scanner sc = new Scanner(System.in);
         char choice, choice2;
